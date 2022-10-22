bf8f4d7fbb76f71572b8d78b2cb3767fcf05506c
bhaa.rizik <bhaa.rizik@imperva.com>
2022-10-22 14:14:33 +0300

    remove favicon

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ const codes = document.querySelectorAll('.code')
 console.log(codes);
 codes[0].focus()
 
-codes.forEach((code, idx) => {
+codes.forEach((code, index) => {
     code.addEventListener('keydown', (e) => {
         console.log(e.key);
-        if (e.key === 'Enter') {
+        if (e.key === 'Enter' && index===5) {
             alert('Your code successfully recieved');
         }
         if(e.key >= 0 && e.key <=9) {
-            codes[idx].value = ''
-            console.log(idx);
-            setTimeout(() => codes[idx + 1].focus(), 10)
+            codes[index].value = '';
+            console.log(index);
+            setTimeout(() => codes[index + 1].focus(), 10)
         } else if(e.key === 'Backspace') {
-            setTimeout(() => codes[idx - 1].focus(), 10)
+            setTimeout(() => codes[index - 1].focus(), 10)
         }
     })
 })
