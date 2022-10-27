110eedcf058ca194679a57fcde171102ce18b0e2
bhaa.rizik <bhaa.rizik@imperva.com>
2022-10-27 09:14:15 +0300

    add more req

--- lab2/index.ts
+++ lab2/index.ts
@@ -1,7 +1,9 @@
 const FirstNumber = document.getElementById('first-number') as HTMLInputElement | null;
 const SecondNumber = document.getElementById('second-number') as HTMLInputElement | null;
 const FinalResult = document.getElementsByClassName('result') as HTMLCollectionOf<Element> | null;
+const HistoryData = document.getElementsByClassName('history') as HTMLCollectionOf<Element> | null;
 const Footer = document.getElementById('footer-style') as HTMLInputElement | null;
+let historyOperation : string[] = [];
 
 const OnSignChoose = (sign: string) => {
   console.log("sign = ", sign);
@@ -24,37 +26,56 @@ const OnSignChoose = (sign: string) => {
       break;
   }
   DisplayResult(result);
+  DisplayHistory(historyOperation);
 }
 
 const Add = () => {
   const first: string = FirstNumber?.value || '0';
   const second: string = SecondNumber?.value || '0';
   console.log('first = ', first, 'second', second)
-  return parseInt(first) + parseInt(second);
+  const result = parseInt(first) + parseInt(second);
+  saveHistory(first, '+', second, result);
+  return result;
 }
 
 const Sub = () => {
   const first: string = FirstNumber?.value || '0';
   const second: string = SecondNumber?.value || '0';
-  return parseInt(first) - parseInt(second);
+  const result = parseInt(first) - parseInt(second);
+  saveHistory(first, '-', second, result);
+  return result;
 }
 
 const Multi = () => {
   const first: string = FirstNumber?.value || '0';
   const second: string = SecondNumber?.value || '0';
-  return parseInt(first) * parseInt(second);
+  const result = parseInt(first) * parseInt(second);
+  saveHistory(first, '*', second, result);
+  return result;
 }
 
 const Div = () => {
   const first: string = FirstNumber?.value || '0';
   const second: string = SecondNumber?.value || '0';
-  return parseInt(first) / parseInt(second);
+  const result = parseInt(first) / parseInt(second);
+  saveHistory(first, '/', second, result);
+  return result;
 }
 
 const DisplayResult = (result: number) => {
   const newLocal = FinalResult?.item(0) || null;
   if (newLocal !== null) {
     newLocal.innerHTML = result.toString();
+  }
+}
+
+const DisplayHistory = (result: string[]) => {
+  const newLocal = HistoryData?.item(0) || null;
+  console.log('newLocal == : result', newLocal, result)
+  if (newLocal !== null) {
+    console.log('newLocal : ', newLocal)
+    newLocal.innerHTML = result.toString();
+    console.log('here : ', newLocal.innerHTML)
   }
 }
 
@@ -67,4 +88,9 @@ const setFoter = () => {
   if (Footer !== null) {
     Footer.innerText = 'Bhaa Rizik @ ' + formattedToday;
   }
+}
+
+const saveHistory = (firstNumber, sign, secondNumber, result) => {
+historyOperation.push(`${firstNumber} ${sign} ${secondNumber} = ${result}    `);
+console.log('history arry: ', historyOperation)
 }
